041f3d4d2923b99f94cbb1e10d89bc7bf3a990b9
lewan-vlg <lewann87@gmail.com>
2024-11-02 22:00:45 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,5 @@
+The_number_of_completed_HW=12
+The_number_of_hours_spent=1.5
+Course_name="Python,"
+Time_for_one_task= The_number_of_hours_spent/The_number_of_completed_HW
+print("курс:",Course_name ,"всего задач:", The_number_of_completed_HW,"затрачено часов:",The_number_of_hours_spent,",", "среднее время выполнения:",Time_for_one_task, "часа.")
