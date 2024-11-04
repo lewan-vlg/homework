c7723a3d449f408b72e7cf2e5f2d4a1a796410f5
lewan-vlg <lewann87@gmail.com>
2024-11-04 21:50:38 +0300

    Add files via upload

---
+++ module_1_5,1.py
@@ -0,0 +1,8 @@
+immutable_var=1,2,3,4,5,6,7,8,9
+print(immutable_var)
+#immutable_var[0]=12 
+print(immutable_var)
+mutable_list=([10,9,8,7,6],5,4,3,2,1,0)
+print(mutable_list)
+mutable_list[0][2]=18
+print(mutable_list)
