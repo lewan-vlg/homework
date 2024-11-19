752f94d1f265f6f68bed7b508b8ca98752e38117
lewan-vlg <lewann87@gmail.com>
2024-11-19 21:23:05 +0300

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,18 +1,15 @@
-def generate_password(n):
-    result = ""
-    pairs = []
-    for i in range(1, 21):
-        for j in range(i + 1, 21):
-            pair_sum = i + j
-            if n % pair_sum == 0:
-                pair_str = f"{i}{j}"
-                if pair_str not in pairs:
-                    pairs.append(pair_str)
-                    result += pair_str
-
-    return result
-
-
-for test_number in range(3, 21):
-    password = generate_password(test_number)
-    print(f"{test_number} - {password}")
+def password(n):
+    result=[]
+    for i in range(1, n):
+        if 3 <i>20:
+            continue
+        for j in range(i+1, n):
+            if n % (i+j)== 0:
+                 result.append((i,j))
+    stroka=""
+    for i,j in result:
+        stroka+=str(i)+str(j)
+    return stroka
+n = int(input("Введите число от 3 до 20: "))
+x= password(n)
+print("Пароль: ", x)
