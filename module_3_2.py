9da04c6f7d945eab2a95506a4f1a099177b75fc4
lewan-vlg <lewann87@gmail.com>
2024-11-20 22:51:32 +0300

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -1,17 +1,18 @@
-def send_email( message, recipient, sender = "university.help@gmail.com"):
-    if any(substring in message for substring in ['@', '.com', '.ru', '.net']):
-        print("Невозможно отправить письмо с адреса", sender, "на адрес", recipient)
+def send_email(message, recipient, sender="university.help@gmail.com"):
+    allowed_endings = (".com", ".ru", ".net")
 
-
-    elif sender == recipient:
-        print("невозможно отправить письмо самому себе!")
-
-    elif sender=="university.help@gmail.com":
-        print("письмо успешно отправлено с адреса", sender, "на адрес", recipient)
+    if sender == recipient:
+        print("Нельзя отправить письмо самому себе!")
+    elif sender.endswith(allowed_endings) and recipient.endswith(allowed_endings):
+        if sender == "university.help@gmail.com":
+            print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}")
+        else:
+            print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}")
     else:
-        print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса", sender, "на адрес", recipient)
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
 
-send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com') 
 
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
 
