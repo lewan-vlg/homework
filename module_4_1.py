79b2239e25cdf149213e4f14327627e38f54a6b5
lewan-vlg <lewann87@gmail.com>
2024-12-01 11:39:31 +0300

    Update module_4_1.py

--- module_4_1.py
+++ module_4_1.py
@@ -1,5 +1,5 @@
-from fake_math import fake_divide
-from true_math import true_divide
+from fake_math import divide as fake_divide
+from true_math import divide as true_divide
 
 result1 = fake_divide(69, 3)
 result2 = fake_divide(3, 0)
